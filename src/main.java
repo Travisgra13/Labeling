83698b05713943179a9e992eb65a55a518cc5618
Travis Graham <travisgra21@gmail.com>
2019-11-19 14:45:02 -0700

    Must Revise

--- src/main.java
+++ src/main.java
@@ -5,6 +5,7 @@ import com.google.gson.JsonParser;
 import org.json.JSONArray;
 
 import java.io.*;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class main {
@@ -13,6 +14,7 @@ public class main {
     static {
         try {
             writer = new PrintWriter("label_outputs.txt", "UTF-8");
+            writer.println("userName, nameOfTest, eventID, bounds, type, allRight, allWrong, typeRightBoundsWrong, boundsRightTypeWrong, NoAttempt, shown");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (UnsupportedEncodingException e) {
@@ -63,6 +65,7 @@ public class main {
             }
 
             int result = new Result(rightType, rightBounds, missed).determineResult();
+
             //goldStandardLabel.getId()
             //id of gold standard or userLabel id
             String bounds = null;
@@ -73,10 +76,13 @@ public class main {
                     bounds = "correct";
                     type = "correct";
                     break;
+                    /*
                 case 2:
-                    bounds = "missed";
-                    type = "missed";
+                    bounds = "noAttempt";
+                    type = "noAttempt";
                     break;
+                    */
+
                 case 3:
                     bounds = "wrong";
                     type = "correct";
@@ -89,14 +95,56 @@ public class main {
                     bounds = "wrong";
                     type = "wrong";
             }
-
-            Output newOutput = new Output(userID, testName, goldStandardLabel.getId(), bounds, type, null);
             if (result != 2) {
+
+                Output newOutput = new Output(userID, testName, goldStandardLabel.getId(), bounds, type, null);
                 outputs.add(newOutput);
             }
 
         }
+        if (outputs.size() == 0) {
+            Output missedOutput = new Output(userID, testName, -1, "NoAttempt", "NoAttempt", null);
+            outputs.add(missedOutput);
+        }
         return outputs;
+    }
+
+    private static ArrayList<Output> consolidateOverlaps(ArrayList<Output> currOutputs) {
+        ArrayList finalList = new ArrayList();
+        for (int i = 0; i < currOutputs.size(); i++) {
+            int currId = currOutputs.get(i).getId();
+            ArrayList<Output> newList = new ArrayList<>();
+            for (int k = i + 1; k < currOutputs.size(); k++) {
+                int peekId = currOutputs.get(k).getId();
+                if (currId == peekId && currId != -1) { //at the end of this if, we can delete the element at peekID from currOutputs and set i -= 1
+                    Output newOutput = new Output(currOutputs.get(i).getUserName(), currOutputs.get(i).getNameOfTest(), currId, "wrong", "correct", currOutputs.get(i).getShown());
+                    if (newList.size() == 0) {
+                        if (currOutputs.get(i).getType().equals("wrong") || currOutputs.get(k).getType().equals("wrong")) {
+                            newOutput.setType("wrong");
+                        }
+                        newList.add(newOutput);
+                    }
+                    else {
+                        if (newList.get(0).getType().equals("wrong") || currOutputs.get(k).getType().equals("wrong")) {
+                            newList.get(0).setType("wrong");
+                        }
+                    }
+                    currOutputs.set(i, newList.get(0));
+                    currOutputs.remove(k);
+                    k--;
+                }
+            }
+            if (newList.size() == 0) {
+                finalList.add(currOutputs.get(i));
+            }
+            else {
+                finalList.add(newList.get(0));
+                i--;
+            }
+
+
+        }
+        return currOutputs;
     }
 
 
@@ -111,10 +159,10 @@ public class main {
     }
 
     private static boolean isOrphan(Label userLabel, Label goldStandardLabel) {
-        if (userLabel.getStartTime() <= goldStandardLabel.getStartTime() && userLabel.getEndTime() <= goldStandardLabel.getStartTime()) {
+        if (userLabel.getStartTime()  < goldStandardLabel.getStartTime() - .5 && userLabel.getEndTime() < goldStandardLabel.getStartTime() - .5) {
             return true;
         }
-        if (userLabel.getStartTime() >= goldStandardLabel.getEndTime() && userLabel.getEndTime() >= goldStandardLabel.getEndTime()) {
+        if (userLabel.getStartTime() > goldStandardLabel.getEndTime() + .5 && userLabel.getEndTime() > goldStandardLabel.getEndTime() + .5) {
             return true;
         }
         return false;
@@ -256,16 +304,55 @@ public class main {
                 String testName = taskName;
                 String id = Integer.toString(newLabel.getId());
                 outputs.addAll(getResult(newLabel, goldStandard, userID, testName, id));
+                outputs = consolidateOverlaps(outputs);
             }
             //All the userLabels
         }
-        writer.println(taskName);
-        writer.println(study.getName());
+        //writer.println(taskName);
+        //writer.println(study.getName());
         for (Output output : outputs) {
             Gson gson = new Gson();
             output.setShown(shown(taskName, study.getName()));
+            int bounds;
+            if (output.getBounds().equals("correct")) {
+                bounds = 1;
+            }
+            else {
+                bounds = 0;
+            }
+            int type;
+            if (output.getType().equals("correct")) {
+                type = 1;
+            }
+            else {
+                type = 0;
+            }
+            int allRight = 0;
+            int allWrong = 0;
+            int typeRightBoundsWrong = 0;
+            int boundsRightTypeWrong = 0;
+            int noAttempt = 0;
+
+            if (output.getId() == -1) {
+                noAttempt = 1;
+            }
+            else if (bounds == 1 && type == 1) {
+                allRight = 1;
+            }
+            else if (bounds == 0 && type == 1) {
+                typeRightBoundsWrong = 1;
+            }
+            else if (bounds == 0 && type == 0) {
+                allWrong = 1;
+            }
+            else if (bounds == 1 && type == 0) {
+                boundsRightTypeWrong = 1;
+            }
+            String myString = output.getUserName() + "," + output.getNameOfTest() + "," + output.getId() + "," + Integer.toString(bounds) + "," + Integer.toString(type) + ","
+                    + Integer.toString(allRight) + "," + Integer.toString(allWrong) + "," + Integer.toString(typeRightBoundsWrong) + "," + Integer.toString(boundsRightTypeWrong)
+                    + "," + Integer.toString(noAttempt) + "," + output.getShown();
             String outputString = gson.toJson(output);
-            writer.println(outputString);
+            writer.println(myString);
         }
     }
 
